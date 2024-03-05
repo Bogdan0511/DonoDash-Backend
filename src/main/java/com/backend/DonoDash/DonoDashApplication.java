c976733d5d5939cba9e1def56e0ab32a0f670c28
Vila Florin-Bogdan <vfir3248@scs.ubbcluj.ro>
2024-03-05 16:10:48 +0200

    Structured the project

---
+++ src/main/java/com/backend/DonoDash/DonoDashApplication.java
@@ -0,0 +1,13 @@
+package com.backend.DonoDash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonoDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonoDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/DonoDash/DonoDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.DonoDash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonoDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
